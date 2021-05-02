ce78015ce705990441418ec27b7bd3ef516e9aa1
SilBin <79167212+SilBin@users.noreply.github.com>
2021-05-02 18:22:16 +0200

    Add files via upload
    Prueba de colisiones de bolas con el suelo incluso la el sonido

---
+++ Assets/Scripts/CambiarColorColision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CambiarColorColision : MonoBehaviour
+{
+
+    private AudioSource sonido1;
+
+    private void Start() {
+        sonido1 = GetComponent<AudioSource> ();
+    }
+
+
+    private void OnCollisionEnter (Collision target)
+    {
+        if(target.gameObject.name.Equals("base") == true ) 
+        {
+            Color randomlySelectedColor = GetRandomColor();
+            GetComponent<Renderer>().material.color = randomlySelectedColor;   
+            sonido1.Play ();
+        }
+    }
+
+    // Update is called once per frame
+    private Color GetRandomColor()
+    {
+        return new Color(
+            r:UnityEngine.Random.Range(0f, 1f),
+            g:UnityEngine.Random.Range(0f, 1f),
+            b:UnityEngine.Random.Range(0f, 1f));
+    }
+}
